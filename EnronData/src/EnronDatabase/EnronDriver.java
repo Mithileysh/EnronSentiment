c2c35946f5d49895eebe90c21d7af621e2021972
Sisi0530 <helenliu0530@gmail.com>
2015-04-22 09:39:19 +1000

    Convert data into csv. file

--- EnronData/src/EnronDatabase/EnronDriver.java
+++ EnronData/src/EnronDatabase/EnronDriver.java
@@ -1,19 +1,20 @@
 /*
- * Version 1.5.3
- * Copyright © , The Apache Software Foundation
- * License and Disclaimer.  The ASF licenses this documentation to you under the Apache License, 
- * Version 2.0 (the "License"); you may not use this documentation except in compliance with the License. 
+ * Copyright 2015 Sisi Liu
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, this documentation and its contents are 
- * distributed under the License on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- * express or implied. See the License for the specific language governing permissions and limitations 
- * under the License.
+    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package EnronDatabase;
 
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -48,12 +49,6 @@ public class EnronDriver {
 	static ArrayList<String> bodyList;
 	static ArrayList<String> tokenList;
 	
-	private static final String COMMA_DELIMITER = ",";
-	private static final String NEW_LINE_SEPARATOR = "\n";
-	//CSV file header
-	private static final String FILE_HEADER = "id,,lastName,gender,age";
-
-	
 	public static void main(String[] args) throws IOException {
 		Connection myConn = null;
 		Statement myStmt = null;
@@ -70,12 +65,19 @@ public class EnronDriver {
 			
 			myStmt = myConn.createStatement();
 			String sql;
-			sql = "SELECT DISTINCT mid, date, body FROM enron.message WHERE subject NOT LIKE 'Re%'OR subject NOT LIKE 'FW%' LIMIT 100 ";
+			sql = "SELECT DISTINCT mid, date, body FROM enron.message WHERE subject NOT LIKE 'Re%'OR subject NOT LIKE ('FW%' OR '%FW%') LIMIT 100 ";
 			ResultSet rs = myStmt.executeQuery(sql);
 			count = 0;
+			String outputFile = "enronemail_svmlight_data.csv";
 			
+			// before we open the file check to see if it already exists
+			//boolean alreadyExists = new File(outputFile).exists();
+			// use FileWriter constructor that specifies open for appending
+			FileWriter fileWriter = null;	
+			fileWriter = new FileWriter (outputFile);
 			
 			while (rs.next()){
+				
 				InputStream tokenmodelIn = new FileInputStream("en-token.bin");
 				InputStream posmodelIn = new FileInputStream("en-pos-maxent.bin");
 				String pathToSWN = "SentiWordNet_3.0.0_20130122.txt";
@@ -91,80 +93,77 @@ public class EnronDriver {
 		        
 		        //System.out.println(bodyList);
 		        try {
-					  TokenizerModel tokenModel = new TokenizerModel(tokenmodelIn);
-					  TokenizerME tokenizer = new TokenizerME(tokenModel);
+		        	
+						
+					TokenizerModel tokenModel = new TokenizerModel(tokenmodelIn);
+					TokenizerME tokenizer = new TokenizerME(tokenModel);
 					  		  
-					  String[] tokens = tokenizer.tokenize(bodyList.get(0));
-					  
-					  POSModel posModel = new POSModel(posmodelIn);
-					  POSTaggerME tagger = new POSTaggerME(posModel);
-					  
-					  System.out.print(id + ", ");
-					  
-					  
-					  for (String token : tokens){
+					String[] tokens = tokenizer.tokenize(bodyList.get(0));
+					double tokenProbs[] = tokenizer.getTokenProbabilities();
+					
+					POSModel posModel = new POSModel(posmodelIn);
+					POSTaggerME tagger = new POSTaggerME(posModel);
+					
+					System.out.print(id + ", ");	
+					
+					
+					fileWriter.append(id + ", ");
+					
+					    
+					for (String token : tokens){
+						
+						tokenList= new ArrayList<String>();
+						tokenList.add(token);
+						attrCount++;  
 						  
-						  
-						  
-						  tokenList= new ArrayList<String>();
-						  tokenList.add(token);
-						  
-						  
-						  List<String> tags = tagger.tag(tokenList);
-						  for(String tag: tags){
-							  
-							  if ( tag.contains("VB")){
-								  
-								  tag = "v";
-								  if (sentiwordnet.extract(token, "v") == 0.0){
-									  tokenList.remove(token);
-								  }else{
-									  System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "v") + ", ");
-							      }
-							  }
-						          
-							  else if (tag.contains("NN")){
-								  tag = "n";
-								  if (sentiwordnet.extract(token, "n") == 0.0){
-									  tokenList.remove(token);
-								  }else{
-									  System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "n") +", ");
-								  }
-								  
-								  
-							  }
-							  else if (tag.contains("JJ")){
-								  tag = "a";
-								  if (sentiwordnet.extract(token, "a") == 0.0){
-									  tokenList.remove(token);
-								  }else{
-									  System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "a") +", ");
-								  }
-								  
-								  
-							  }
-							  else if (tag.contains("RB")){
-								  tag = "r";
-								  if (sentiwordnet.extract(token, "r") == 0.0){
-									  tokenList.remove(token);
-								  }else{
-									  System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "r") +", ");
-								  }
-								  
-								  
-							  }
-							  else{
-								  tokenList.remove(token);
-								  
-							  }
-							 
-							  
-						  }
-						  
-					  }
-						  
-					  System.out.println("");
+						List<String> tags = tagger.tag(tokenList);
+						for(String tag: tags){
+							if ( tag.contains("VB")){
+								tag = "v";
+								if (sentiwordnet.extract(token, "v") == 0.0){
+									tokenList.remove(token);
+								}else{
+									//System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "v") + ", ");
+							        fileWriter.append(sentiwordnet.extract(token, "v") + ", " );
+								}
+							}
+							else if (tag.contains("NN")){
+								tag = "n";
+								if (sentiwordnet.extract(token, "n") == 0.0){
+									tokenList.remove(token);
+								}else{
+									//System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "n") +", ");
+									fileWriter.append(sentiwordnet.extract(token, "n") + ", " );
+								}
 				  
+							}
+							else if (tag.contains("JJ")){
+								tag = "a";
+								if (sentiwordnet.extract(token, "a") == 0.0){
+									tokenList.remove(token);
+								}else{
+									//System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "a") +", ");
+									fileWriter.append(sentiwordnet.extract(token, "a") + ", " );
+								} 
+							}
+							else if (tag.contains("RB")){
+								tag = "r";
+								if (sentiwordnet.extract(token, "r") == 0.0){
+									tokenList.remove(token);
+								}else{
+									//System.out.print(token + "/ "+ tag + ": " + sentiwordnet.extract(token, "r") +", ");
+									fileWriter.append(sentiwordnet.extract(token, "r") + ", " );
+								}  
+							}
+							else{
+								tokenList.remove(token);								  
+							}	 
+						}
+						
+					}
+					//System.out.println("");
+					fileWriter.append("\n");
+					//}
 					}
 					catch (IOException e) {
 					  e.printStackTrace();
@@ -184,14 +183,19 @@ public class EnronDriver {
 						   catch (IOException e) {
 						}
 					  }
+					  
 					}
 				
 				count ++;
 				
-				//System.out.print(tokenList.toString()  );
-				
+				//System.out.print(tokenList.toString()  );				
 			}
-			
+			try{
+				  fileWriter.flush();
+				  fileWriter.close();
+			  }catch (IOException e) {
+				  e.printStackTrace();
+			  }
 			
 			rs.close();
 			myStmt.close();
